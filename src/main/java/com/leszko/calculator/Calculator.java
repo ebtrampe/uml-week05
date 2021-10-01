30db62c05baf62eb1a2cad266922e7b346e21e31
Ei <ebtrampe@gmail.com>
2021-10-01 13:50:56 +0800

    Add javadoc

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service Some Javadoc
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
