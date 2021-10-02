8f9b96cd9490955e15b1027ba4a3b4d5ff521ee0
Ei <ebtrampe@gmail.com>
2021-10-02 09:25:54 +0800

    Revert for Docker desktop retry

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,11 +3,6 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
-* Some javadoc
-@author Ei
-@version 1
-*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
